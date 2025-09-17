4f65275cd3b519a72616ac87cb7a721b75372a68
Aditya Nisal <72440232+Adya97@users.noreply.github.com>
2025-09-18 01:24:34 +0200

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -1,20 +1,31 @@
 terraform {
   required_version = "~> 1.13.3"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.49.0"
     }
   }
+
   backend "s3" {
-    bucket         = "dev-adya-tf-bucket"
-    region         = "eu-central-1"
-    key            = "eks/terraform.tfstate"
-    dynamodb_table = "Lock-Files"
-    encrypt        = true
+    bucket       = "dev-adya-tf-bucket"
+    region       = "eu-central-1"
+    key          = "eks/terraform.tfstate"
+    encrypt      = true
+    use_lockfile = true   # replaces deprecated `dynamodb_table`
+    # If you previously used workspaces with a prefix, uncomment:
+    # workspaces { prefix = "env" }
   }
 }
 
+# NOTE: variable names can't contain hyphens. Use `aws_region`.
 provider "aws" {
-  region = var.aws-region
+  region = var.aws_region
+}
+
+variable "aws_region" {
+  description = "AWS region for provider"
+  type        = string
+  default     = "eu-central-1"
 }
