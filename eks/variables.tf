d2b4d6c5e28837f6bf6fc1c53af0bdc431c1e429
Aditya Nisal <72440232+Adya97@users.noreply.github.com>
2025-09-18 09:40:44 +0200

    Update variables.tf

--- eks/variables.tf
+++ eks/variables.tf
@@ -1,8 +1,7 @@
-variable "aws_region" {}
-variable "env" {}
 variable "cluster_name" {}
-variable "vpc_cidr_block" {}
+variable "cidr_block" {}
 variable "vpc_name" {}
+variable "env" {}
 variable "igw_name" {}
 
 variable "pub_subnet_count" { type = number }
@@ -19,11 +18,22 @@ variable "eip_name" {}
 variable "ngw_name" {}
 variable "eks_sg" {}
 
+# IAM toggles
+variable "is_eks_role_enabled"          { type = bool }
+variable "is_eks_nodegroup_role_enabled" { type = bool }
+
 # EKS
 variable "is_eks_cluster_enabled" { type = bool }
 variable "cluster_version" {}
 variable "endpoint_private_access" { type = bool }
 variable "endpoint_public_access"  { type = bool }
+
+variable "addons" {
+  type = list(object({
+    name    = string
+    version = string
+  }))
+}
 
 variable "ondemand_instance_types" { type = list(string) }
 variable "spot_instance_types"     { type = list(string) }
@@ -34,10 +44,3 @@ variable "max_capacity_on_demand"     { type = number }
 variable "desired_capacity_spot"      { type = number }
 variable "min_capacity_spot"          { type = number }
 variable "max_capacity_spot"          { type = number }
-
-variable "addons" {
-  type = list(object({
-    name    = string
-    version = string
-  }))
-}
